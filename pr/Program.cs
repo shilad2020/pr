a249acd9cb8ddac49e56d31f12af88aecab428f8
shilad2020 <shilad2020@gmail.com>
2025-02-16 18:09:56 -0700

    Add project files.

---
+++ pr/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CosoleApp1
+{
+	public enum Fruits
+	{
+        Apple, Mangno, Orange
+    }
+	internal class Program
+	{
+		static	void Main(string[] args)
+		{
+             Console.WriteLine("Hello, World!");
+            var ch = Console.ReadLine();
+            Fruits fruits = new Fruits();
+          //  Maths maths = new Maths();
+            switch (fruits)
+            {
+                case Fruits.Apple: break;
+                case Fruits.Mangno: break;
+                case Fruits.Orange: break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
